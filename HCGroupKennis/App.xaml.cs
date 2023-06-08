518b0a501302eb88f33f94051ec74b4003d82d92
Marijn17S <59451866+Marijn17S@users.noreply.github.com>
2023-06-08 19:25:46 +0200

    initial

---
+++ HCGroupKennis/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace HCGroupKennis
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
